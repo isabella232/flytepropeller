784ab38d46414c637d72def07d1eb6dbec1caeef
Anand Swaminathan <aswaminathan@lyft.com>
2020-11-18 17:03:22 -0800

    Fix node execution for tasks in new events

--- pkg/controller/nodes/task/handler_test.go
+++ pkg/controller/nodes/task/handler_test.go
@@ -446,6 +446,8 @@ func Test_task_Handle_NoCatalog(t *testing.T) {
 
 		executionContext := &mocks.ExecutionContext{}
 		executionContext.OnGetExecutionConfig().Return(v1alpha1.ExecutionConfig{})
+		executionContext.OnGetEventVersion().Return(v1alpha1.EventVersion0)
+		executionContext.OnGetParentInfo().Return(nil)
 		nCtx.OnExecutionContext().Return(executionContext)
 
 		st := bytes.NewBuffer([]byte{})
@@ -768,6 +770,8 @@ func Test_task_Handle_Catalog(t *testing.T) {
 
 		executionContext := &mocks.ExecutionContext{}
 		executionContext.OnGetExecutionConfig().Return(v1alpha1.ExecutionConfig{})
+		executionContext.OnGetEventVersion().Return(v1alpha1.EventVersion0)
+		executionContext.OnGetParentInfo().Return(nil)
 		nCtx.OnExecutionContext().Return(executionContext)
 
 		nCtx.OnRawOutputPrefix().Return("s3://sandbox/")
@@ -992,6 +996,8 @@ func Test_task_Handle_Barrier(t *testing.T) {
 
 		executionContext := &mocks.ExecutionContext{}
 		executionContext.OnGetExecutionConfig().Return(v1alpha1.ExecutionConfig{})
+		executionContext.OnGetEventVersion().Return(v1alpha1.EventVersion0)
+		executionContext.OnGetParentInfo().Return(nil)
 		nCtx.OnExecutionContext().Return(executionContext)
 
 		nCtx.OnRawOutputPrefix().Return("s3://sandbox/")
